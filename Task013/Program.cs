1e1c21e792da9473431a078111e9e243f3218834
Tra-Ta <madtazik@gmail.com>
2022-11-21 21:30:56 +0300

    Complete Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number -->");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThreeDigit(int num)
+{
+    int theFirstAction = num / 100;
+    int secondAction = theFirstAction % 10;
+    int result = secondAction;
+    return result;
+}
+int threeDigit = ThreeDigit(number);
+
+if (number < 100) Console.WriteLine("There is no third digit");
+else Console.WriteLine($"The second digit of the number --> {threeDigit}");
