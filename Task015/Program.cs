43335abcc865020674b7dd4fb406d36a694a8df2
Tra-Ta <madtazik@gmail.com>
2022-11-22 00:22:33 +0300

    Complete Tasko15

--- Task015/Program.cs
+++ Task015/Program.cs
@@ -1,13 +1,21 @@
 ﻿Console.WriteLine("Enter digit ->");
 int digit =Convert.ToInt32(Console.ReadLine());
 
-bool MultiplicityTwoDigits (int num)
+bool CheckingWeekend (int num)
 {
-   return num % 7 == 0 && num % 23 == 0;
+   if(num == 1) Console.Write("Monday");
+else if(num == 2) Console.Write("Tuesday");
+else if(num == 3) Console.Write("Wednesday");
+else if(num == 4) Console.Write("Thursday");
+else if(num == 5) Console.Write("Friday");
+else if(num == 6) Console.Write("Saturday");
+else if(num == 7) Console.Write("Sunday");
+return num > 5;
 }
 
-bool result = MultiplicityTwoDigits(number);
+bool result = CheckingWeekend (digit);
 
-string resultStr = result ? "ДА" : "НЕТ";
+if (digit > 7 )Console.WriteLine("Out diaposon");  
 
-Console.WriteLine($"{number} weekend?-> {resultStr}");
+else Console.WriteLine($" weekend?-> {result}");
+
