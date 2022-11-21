f5046f2bb086e01cda4343862e9a43e3c6a300f0
Tra-Ta <madtazik@gmail.com>
2022-11-21 21:25:04 +0300

    Complete Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter three-digit number ->");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MiddleDigit(int num)
+{
+    int theFirstAction = num / 10;
+    int secondAction = theFirstAction % 10;
+    int result = secondAction;
+    return result;
+}
+int middleDigit = MiddleDigit(number);
+
+if (number > 999 || number < 100) Console.WriteLine("The number is not three-digit");
+else Console.WriteLine($"The second digit of the number --> {middleDigit}");
